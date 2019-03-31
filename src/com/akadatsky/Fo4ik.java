ba7ae02d4f341b77714016589d8a83eb59418a7b
Vlad Zinkovsky <terraemw@gmail.com>
2019-03-31 21:07:37 +0300

    fix bugs

--- src/com/akadatsky/Fo4ik.java
+++ src/com/akadatsky/Fo4ik.java
@@ -1,11 +1,11 @@
 package com.akadatsky;
 
 public class Fo4ik {
-    public static void doinSomething{
+    public static void doinSomething(){
 
     }
 
-    public static void getLogin{
+    public static void getLogin(){
 
     }
 }
