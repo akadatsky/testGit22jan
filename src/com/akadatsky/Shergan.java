62e96a4ae0548a9b74ff9678e69ddfd4f218d3b7
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-01 23:06:30 +0300

    add methods

--- src/com/akadatsky/Shergan.java
+++ src/com/akadatsky/Shergan.java
@@ -1,4 +1,34 @@
 package com.akadatsky;
 
 public class Shergan {
+    private static int age;
+    private static String name;
+    private static String group;
+
+    public Shergan() {
+    }
+
+    public static int getAge() {
+        return age;
+    }
+
+    public static void setAge(int age) {
+        Shergan.age = age;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Shergan.name = name;
+    }
+
+    public static String getGroup() {
+        return group;
+    }
+
+    public static void setGroup(String group) {
+        Shergan.group = group;
+    }
 }
