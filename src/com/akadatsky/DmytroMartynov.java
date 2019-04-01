99eb10dea798f0252120f44992ec068fc1602256
Dima Martynov <dmytro.martynov96@gmail.com>
2019-04-01 23:20:55 +0300

    add burger methods:D

--- src/com/akadatsky/DmytroMartynov.java
+++ src/com/akadatsky/DmytroMartynov.java
@@ -1,4 +1,27 @@
 package com.akadatsky;
 
 public class DmytroMartynov {
+    String nameOfBurger;
+    int massOfBurger;
+
+    public int getMassOfBurger() {
+        return massOfBurger;
+    }
+
+    public void setMassOfBurger(int massOfBurger) {
+        this.massOfBurger = massOfBurger;
+    }
+
+    public void setNameOfBurger(String nameOfBurger) {
+        this.nameOfBurger = nameOfBurger;
+    }
+
+    public String getNameOfBurger() {
+        return nameOfBurger;
+    }
+
+    public void createBurger(int size){
+        System.out.println("Wanted size of burger: ?" + size);
+    }
+
 }
