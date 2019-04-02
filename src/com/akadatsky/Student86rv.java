47f619a9002f4ea6370b33297df4a5e1349338ca
Roman <student.rv86@gmail.com>
2019-04-02 17:07:45 +0300

    add class Student86rv

--- src/com/akadatsky/Student86rv.java
+++ src/com/akadatsky/Student86rv.java
@@ -1,4 +1,16 @@
 package com.akadatsky;
 
 public class Student86rv {
+    private String name;
+    private int age;
+    private String email;
+
+    public Student86rv() {
+    }
+
+    public Student86rv(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
 }
