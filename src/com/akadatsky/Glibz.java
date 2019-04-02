0e7590750c65a38f4cfd3f7bfe1b1d4051310f4a
Glib <zamikula.glib@gmail.com>
2019-04-02 17:38:20 +0300

    THe first version of Glibz class

--- src/com/akadatsky/Glibz.java
+++ src/com/akadatsky/Glibz.java
@@ -1,4 +1,19 @@
 package com.akadatsky;
 
 public class Glibz {
+    String vesselName;
+    int vesselCode;
+
+    public Glibz(String vesselName, int vesselCode) {
+        this.vesselName = vesselName;
+        this.vesselCode = vesselCode;
+    }
+
+    public String getVesselName() {
+        return vesselName;
+    }
+
+    public int getVesselCode() {
+        return vesselCode;
+    }
 }
