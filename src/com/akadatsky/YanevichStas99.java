20fb44d523d5599ab0291fd39f61ecdfa0a803ed
Yanevich Stas <syanevich1199@gmail.com.ua>
2019-04-03 17:17:05 +0300

    add find max value nethod

--- src/com/akadatsky/YanevichStas99.java
+++ src/com/akadatsky/YanevichStas99.java
@@ -8,4 +8,13 @@ public class YanevichStas99 {
         Arrays.sort(array);
         return array;
     }
+    public int maxInArray(int[] array){
+        int max=0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i]>max){
+                max=array[i];
+            }
+        }
+        return max;
+    }
 }
