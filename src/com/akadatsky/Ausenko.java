a6c933e535cb792e997739e8926d74c1ca4ed2b1
ausenko <anton.usenko87@gmail.com>
2019-04-03 22:02:10 +0300

    Ausenko

--- src/com/akadatsky/Ausenko.java
+++ src/com/akadatsky/Ausenko.java
@@ -1,4 +1,36 @@
 package com.akadatsky;
 
 public class Ausenko {
+    private static int age;
+    private static String name;
+    private static String group;
+
+    public Ausenko() {
+    }
+
+    public static int getAge() {
+        return age;
+    }
+
+    public static void setAge(int age) {
+        Ausenko.age = age;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Ausenko.name = name;
+    }
+
+    public static String getGroup() {
+        return group;
+    }
+
+    public static void setGroup(String group) {
+        Ausenko.group = group;
+    }
 }
+
+
