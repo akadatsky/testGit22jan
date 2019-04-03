27d29702761393f4fa2a431915c0068002607555
Yanevich Stas <syanevich1199@gmail.com.ua>
2019-04-03 17:15:16 +0300

    add sort method

--- src/com/akadatsky/YanevichStas99.java
+++ src/com/akadatsky/YanevichStas99.java
@@ -1,5 +1,11 @@
 package com.akadatsky;
 
+import java.util.Arrays;
+
 public class YanevichStas99 {
     private int[] arr={7,45,89,12,64,68,25,0,1,36,99};
+    public int[] sortArray(int[] array){
+        Arrays.sort(array);
+        return array;
+    }
 }
